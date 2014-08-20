3d9d8c89ebbf5b7f5ae76ea83271d80358ffc7f4
Tony Lee <github@roganartu.com>
2014-08-21 03:20:40 +1000

    Extract season and episode from filename

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/cli"
+	"io/ioutil"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
@@ -17,9 +20,28 @@ func main() {
 			Name:  "parse",
 			Usage: "parse the subtitle files and seed the database",
 			Action: func(c *cli.Context) {
-				fmt.Println("Parsing subtitle files and seeding database with possible fragments")
+				parse()
 			},
 		},
 	}
 	app.Run(os.Args)
+}
+
+func parse() {
+	fmt.Println("Parsing subtitle files and seeding database with possible fragments")
+
+	files, err := ioutil.ReadDir("subtitles")
+	if err != nil {
+		fmt.Printf("There was an error listing the files in the subtitles directory: ", err)
+	}
+
+	for _, f := range files {
+		// Extract the title and episode from the filename
+		metadata := regexp.MustCompile("([0-9])x([0-9]{2}) - (.+)\\.en\\.srt").FindStringSubmatch(f.Name())
+		season, _ := strconv.Atoi(metadata[1])
+		episode, _ := strconv.Atoi(metadata[2])
+		title := metadata[3]
+
+		fmt.Printf("Parsing Season %d, Episode %d: %s\n", season, episode, title)
+	}
 }
