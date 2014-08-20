71b5701f3a394ab2017dc9d6c5562a8bbcdf1fbf
Tony Lee <github@roganartu.com>
2014-08-21 04:35:32 +1000

    Parse subtitle files for lines and metadata

--- main.go
+++ main.go
@@ -7,7 +7,22 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
+
+type episode struct {
+	seasonNum  int
+	episodeNum int
+	title      string
+	lines      []*line
+}
+
+type line struct {
+	order   int
+	start   int
+	end     int
+	content string
+}
 
 func main() {
 	app := cli.NewApp()
@@ -35,13 +50,76 @@ func parse() {
 		fmt.Printf("There was an error listing the files in the subtitles directory: ", err)
 	}
 
+	empty := regexp.MustCompile("^\\s$")
+	count := 0
+
 	for _, f := range files {
 		// Extract the title and episode from the filename
 		metadata := regexp.MustCompile("([0-9])x([0-9]{2}) - (.+)\\.en\\.srt").FindStringSubmatch(f.Name())
-		season, _ := strconv.Atoi(metadata[1])
-		episode, _ := strconv.Atoi(metadata[2])
-		title := metadata[3]
+		ep := &episode{
+			title: metadata[3],
+		}
+		ep.seasonNum, _ = strconv.Atoi(metadata[1])
+		ep.episodeNum, _ = strconv.Atoi(metadata[2])
 
-		fmt.Printf("Parsing Season %d, Episode %d: %s\n", season, episode, title)
+		fmt.Printf("Parsing Season %d, Episode %d: %s\n", ep.seasonNum, ep.episodeNum, ep.title)
+
+		// Get the file contents
+		byte_contents, err := ioutil.ReadFile("subtitles/" + f.Name())
+		if err != nil {
+			fmt.Printf("There was an error reading the subtitle file: %s\n", err)
+		}
+		contents := string(byte_contents)
+
+		// Subtitle files are structured nicely using newlines
+		segments := regexp.MustCompile("\n\n+").Split(contents, -1)
+		for _, segment := range segments {
+			rows := strings.Split(segment, "\n")
+			l := &line{}
+			l.order, err = strconv.Atoi(rows[0])
+			l.start, l.end = timestampToMilliseconds(rows[1])
+			l.content = ""
+			for i := 2; i < len(rows); i++ {
+				l.content += " " + rows[i]
+			}
+
+			// Empty subtitles hold no information
+			if empty.MatchString(l.content) {
+				continue
+			}
+
+			ep.lines = append(ep.lines, l)
+			count += len(strings.Split(l.content, " "))
+		}
 	}
+	fmt.Printf("Word Count: %d\n", count)
+}
+
+func timestampToMilliseconds(str string) (int, int) {
+	// This is a very error prone process. Not all rows have timestamp data
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+			fmt.Printf("str: %s\n", str)
+		}
+	}()
+
+	r := regexp.MustCompile("([0-9]*):([0-9]*):([0-9]*),([0-9]*)")
+	matches := r.FindAllStringSubmatch(str, -1)
+
+	// Start time in milliseconds
+	hours, _ := strconv.Atoi(matches[0][0])
+	minutes, _ := strconv.Atoi(matches[0][1])
+	seconds, _ := strconv.Atoi(matches[0][2])
+	milliseconds, _ := strconv.Atoi(matches[0][3])
+	start := hours*3600000 + minutes*60000 + seconds*1000 + milliseconds
+
+	// End time in milliseconds
+	hours, _ = strconv.Atoi(matches[0][0])
+	minutes, _ = strconv.Atoi(matches[0][1])
+	seconds, _ = strconv.Atoi(matches[0][2])
+	milliseconds, _ = strconv.Atoi(matches[0][3])
+	end := hours*3600000 + minutes*60000 + seconds*1000 + milliseconds
+
+	return start, end
 }
